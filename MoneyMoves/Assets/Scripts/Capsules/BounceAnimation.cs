119a5ffdf90edd2a8134473dc034e7f017f59616
TychoDesktop <TychoZaal@gmail.com>
2020-04-28 16:57:11 +0200

    Bouncy jukebox + particles. Slightly cleaned up folders

---
+++ MoneyMoves/Assets/Scripts/Capsules/BounceAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceAnimation : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    float bpm;
+
+    private void Start()
+    {
+        animator.SetTrigger("Start");
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", bpm);
+    }
+}
--- MoneyMoves/Assets/Scripts/Capsules/Grid/GridCell.cs
+++ MoneyMoves/Assets/Scripts/Capsules/Grid/GridCell.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 public class GridCell : MonoBehaviour
 {
     public int vertical, horizontal;

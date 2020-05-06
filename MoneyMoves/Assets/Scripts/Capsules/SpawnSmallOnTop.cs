71dabfc6f67b4dc1bc4964c072ce37b901a08be0
TychoDesktop <TychoZaal@gmail.com>
2020-05-06 13:11:30 +0200

    Glass spawning

---
+++ MoneyMoves/Assets/Scripts/Capsules/SpawnSmallOnTop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSmallOnTop : MonoBehaviour
+{
+    [SerializeField]
+    private int amountToSpawn = 1;
+
+    [SerializeField]
+    private List<GameObject> objectsToSpawn;
+
+    [SerializeField]
+    private Vector3 edge1, edge2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int toSpawn = Random.Range(0, amountToSpawn + 1);
+
+        for (int i = 0; i < toSpawn; i++)
+        {
+            int rand = Random.Range(0, objectsToSpawn.Count);
+            float xPos = Random.Range(edge1.x, edge2.x);
+            float zPos = Random.Range(edge1.z, edge2.z);
+
+            Vector3 randomPositions = new Vector3(xPos, 0, zPos);
+            Vector3 position = transform.position + randomPositions;
+
+            GameObject smallObj = Instantiate(objectsToSpawn[rand], position, Quaternion.identity);
+            smallObj.transform.parent = transform;
+        }
+    }
+}

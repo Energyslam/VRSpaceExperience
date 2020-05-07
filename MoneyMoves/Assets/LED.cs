6c2567a52c1455312dfe0c8be5415f730ba1a457
TychoDesktop <TychoZaal@gmail.com>
2020-05-07 16:29:07 +0200

    LED Strip & Wall objects disabling upon opening door

---
+++ MoneyMoves/Assets/LED.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LED : WallObject
+{
+    [SerializeField]
+    List<Material> ledMaterials = new List<Material>();
+
+    MeshRenderer rend;
+
+    [SerializeField]
+    Material targetMaterial;
+
+    [SerializeField]
+    [ColorUsage(true, true)]
+    Color col1, col2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+
+        for (int i = 0; i < rend.materials.Length - 1; i++)
+        {
+            ledMaterials.Add(rend.materials[i]);
+            float randomTime = Random.Range(0, 2);
+            if (randomTime < 0.5f) randomTime = 0.5f;
+            StartCoroutine(ChangeColor(ledMaterials[i], randomTime));
+        }
+    }
+
+    private IEnumerator ChangeColor(Material mat, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Color col = mat.GetColor("_BaseColor") == col1 ? col2 : col1;
+        mat.SetColor("_BaseColor", col);
+        mat.SetColor("_EmissionColor", col);
+        StartCoroutine(ChangeColor(mat, waitTime));
+    }
+
+    public override void NotifyWallChange(bool isClosed)
+    {
+        rend.enabled = isClosed;
+    }
+}
--- MoneyMoves/Assets/Scripts/Capsules/CapsuleAnimations.cs
+++ MoneyMoves/Assets/Scripts/Capsules/CapsuleAnimations.cs
@@ -9,12 +9,34 @@ public class CapsuleAnimations : MonoBehaviour
 
     public GameObject distancePivot;
 
+    public bool isClosed = true;
+
+    [SerializeField]
+    public List<WallObject> wallObjects;
+
     [SerializeField]
     private float animationSpeed = 1;
+
+    private void Start()
+    {
+        isClosed = animationSpeed > 0 ? true : false;
+
+        for (int i = 0; i < wallObjects.Count; i++)
+        {
+            wallObjects[i].NotifyWallChange(isClosed);
+        }
+    }
 
     public void Animate(bool hasToOpen)
     {
         float speed = hasToOpen ? animationSpeed : -animationSpeed;
+        isClosed = speed > 0 ? true : false;
+
+        for (int i = 0; i < wallObjects.Count; i++)
+        {
+            wallObjects[i].NotifyWallChange(isClosed);
+        }
+
         animator.SetFloat("Speed", speed);
 
         animator.SetTrigger("OpeningTrigger");

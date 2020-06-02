2b1aeff7213ae706c58cf88117e5b4b7af185a50
TychoDesktop <TychoZaal@gmail.com>
2020-06-02 17:11:42 +0200

    Colliders added for beach objects. Imported cat cafe models. Basic setup of cat cafe working

---
+++ MoneyMoves/Assets/Scripts/Misc/RandomScaleOnStartup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScaleOnStartup : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 minScale, maxScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(Random.Range(minScale.x, maxScale.x), Random.Range(minScale.y, maxScale.y), Random.Range(minScale.z, maxScale.z));
+    }
+}

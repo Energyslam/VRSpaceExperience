75c6417c278d6ea9a500e64c1d20dff7479154f4
TychoDesktop <TychoZaal@gmail.com>
2020-06-08 15:45:42 +0200

    Wall colliders added that are removed when the door opens. Added area gizmo for events

---
+++ MoneyMoves/Assets/Scripts/Capsules/Procedural Content Generation/Content Inside/WallCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollider : WallObject
+{
+    public override void NotifyWallChange(bool isOpen)
+    {
+        gameObject.SetActive(isOpen);
+    }
+}

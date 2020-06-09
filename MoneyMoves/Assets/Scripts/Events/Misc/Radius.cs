667b41aa1da68347d3a9988333734b0ba96dbb03
TychoDesktop <TychoZaal@gmail.com>
2020-06-09 13:35:24 +0200

    Event radius setup

---
+++ MoneyMoves/Assets/Scripts/Events/Misc/Radius.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Radius : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject areaPrefab;
+    private GameObject eventArea;
+
+    [SerializeField]
+    private float xRadius = 1.0f, yRadius = 1.0f, zRadius = 1.0f;
+
+    void OnEnable()
+    {
+        if (eventArea == null)
+        {
+            if (GetComponentInChildren<Area>() == null)
+            {
+                eventArea = Instantiate(areaPrefab, Vector3.zero, Quaternion.identity, transform);
+            }
+            else
+            {
+                eventArea = GetComponentInChildren<Area>().gameObject;
+            }
+        }
+    }
+
+    void OnValidate()
+    {
+        if (eventArea != null)
+            eventArea.transform.localScale = new Vector3(xRadius, yRadius, zRadius);
+    }
+}
